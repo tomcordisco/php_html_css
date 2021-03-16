0b87b52bb2ca98e1dc6ed7d9caf28d1d571b9e93
Tomás Cordisco <58890694+tomcordisco@users.noreply.github.com>
2021-03-16 13:27:18 -0300

    Update registry_page.php

--- registry_page.php
+++ registry_page.php
@@ -26,6 +26,6 @@
         <input class="form" type="submit" value="Home">
     </form><br><br>
 
-<h4>Copyright © 2020 Cordisco's Company. All rights reserved. Contact: tom.cordisco2002@gmail.com</h4>
+<h4>Copyright © 2020 Cordisco's Company. All rights reserved.</h4>
 </body>
 </html>
