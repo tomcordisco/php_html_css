8def539eaa541ffbbb55cc8df0599b00332e073c
Tomás Cordisco <58890694+tomcordisco@users.noreply.github.com>
2021-03-16 13:28:31 -0300

    Update query.php

--- query.php
+++ query.php
@@ -36,6 +36,6 @@
         <input class="form" type="submit" value="Back">
     </form><br>
 
-<h4>Copyright © 2020 Cordisco's Company. All rights reserved. Contact: tom.cordisco2002@gmail.com</h4>
+<h4>Copyright © 2020 Cordisco's Company. All rights reserved.</h4>
 </body>
 </html>
